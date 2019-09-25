fcd9c63c186f58d028b2e2ff9427643754d48924
Adrian Chadd <adrian@freebsd.org>
2019-09-25 14:09:31 -0700

    Undo the last commit; the max framesize should be 7 not 8. That will also fix this particular bug.

--- lapb.c
+++ lapb.c
@@ -381,21 +381,7 @@ uint n
 	 * then we have a frame reject condition.
 	 */
 	oldest = (axp->vs - axp->unack) & MMASK;
-	while(axp->unack != 0) {
-		/*
-		 * If oldest == n and unack is 8, then we've received
-		 * all 8 frames in our transmit queue and should handle
-		 * them here.
-		 *
-		 * Otherwise we will forever keep looping over transmitting
-		 * the same 8 frames until we miss one, or we get an
-		 * intermediate RR for less than 8 frames, leading to a
-		 * partial flush.
-		 */
-		if ((oldest == n) && (axp->unack != 8)) {
-			break;
-		}
-
+	while(axp->unack != 0 && oldest != n){
 		if((bp = dequeue(&axp->txq)) == NULL){
 			/* Acking unsent frame */
 			return -1;
